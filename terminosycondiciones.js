bbef3ad812af99a47ed07f7ebd942dbf19b0c355
Danielsanch0 <146590124+Danielsanch0@users.noreply.github.com>
2023-10-19 20:57:25 -0500

    cancelar

--- terminosycondiciones.js
+++ terminosycondiciones.js
@@ -6,7 +6,10 @@ function aceptarTerminos() {
     alert("Has aceptado los términos y condiciones.");
     window.location.href = "creditos.html";
 }
-
+function cancelarPopup() {
+    alert("Has Cancelado los términos y condiciones.");
+    window.location.href = "Formulario.html";
+}
 function cerrarPopup() {
     document.getElementById("popup").style.display = "none";
 }
