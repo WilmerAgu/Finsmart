2f55ed686e11159c96d07eff7f76d6d8e69d396e
wilmerAgu <wilmer.com.co@gmail.com>
2023-10-21 15:00:14 -0500

    cambiando estilos

--- creditos.js
+++ creditos.js
@@ -1,26 +1,7 @@
-const btntoggle = document.querySelector('.toggle-btn')
-
-btntoggle.addEventListener('click', function(){
-    document.getElementById('sidebar').classList.toggle('active');
+document.getElementById("openModal").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "block";
 });
 
-
-function abrirModal(modalId){
-    var modal = document.getElementById('modal-' + modalId);
-    modal.style.display = 'block';
-}
-
-function cerrarModal(modalId){
-    var modal = document.getElementById('modal-' + modalId);
-    modal.style.display = 'none';
-}
-
-var enlaces = document.querySelectorAll('.abrir-modal');
-
-enlaces.forEach(function(enlace) {
-    enlace.addEventListener('click', function(e) {
-        e.preventDefault();
-        var modalId = this.getAttribute('data-modal');
-        abrirModal(modalId);
-    });
+document.getElementById("closeModal").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
 });
